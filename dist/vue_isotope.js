799231002e59123d741d5c748029e8e5d7b0213d
desmaisons_david <desmaisons_david@hotmail.com>
2017-03-19 22:34:36 -0300

    preparing next version

--- dist/vue_isotope.js
+++ dist/vue_isotope.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 (function () {
   function buildVueIsotope(_, Isotope) {
 
